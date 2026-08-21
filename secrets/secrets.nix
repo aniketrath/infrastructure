5b5d72f353cdf150f99c4ea350d4a380f997e5ba
Aniket Rath <aniketrath1121@gmail.com>
2026-08-21 21:25:41 +0530

    k3s: dedicated kubectl group, drop redundant persistence entries;...

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,18 @@
 let
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcEPHCU24dDL+IxHMU8djT199vQWvwNOt2RL1enWabl aniketrath1121@gmail.com";
-  midguard-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINZhH6apiZ7YtHSRI7CKvCdhL6QzRbJm+vnfTsRTNR5V root@midguard-01";
-  midguard-02 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFEiNJE3Yz3edUmgUdkMmzIg4KByOX3JsBEXcgRaKzIz root@midguard-02";
+  midguard-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7amCnWmLmYCVbnNt/LYQ2jYcxlFCdQLLyo7k9UW4Z6 root@midguard-01";
+  midguard-02 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUrqUKpQuKEtqHUjCDQNYQ/4OdYgZC+3++Gd0ZzO0BJ root@midguard-02";
 
   adminKeys = [ laptop midguard-01 midguard-02 ];
   clusterNodes = [ midguard-01 midguard-02 ];
-  tokenKeys = [ laptop ] ++ clusterNodes;
+  tokenKeys = clusterNodes;
 in
 {
   "usercreds_homelabadmin.age".publicKeys = adminKeys;
-  "clustercreds_k3s.age".publicKeys = tokenKeys;
-  "clustercreds_postgres.age".publicKeys = tokenKeys;
-  "clustercreds_tailscale.age".publicKeys = tokenKeys;
+  "clustercreds_k3s.age".publicKeys = clusterNodes;
+  "clustercreds_postgres.age".publicKeys = clusterNodes;
+  "clustercreds_tailscale.age".publicKeys = clusterNodes;
+  "clustercreds_infisical.age".publicKeys = clusterNodes;
+  
+  __access_keys = adminKeys;
 }
